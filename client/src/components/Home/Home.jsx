711311cb588307050207933d111b2e97e771f7b4
Ashley Pean <pean.ashley@gmail.com>
2021-03-13 18:07:22 -0500

    completed basic layout of code snippet card

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import { Container, Title, Sidecard } from './Home.styles'
+import { PostsContainer, Title, Sidecard, Post, Code, Username, Pre, Likes, Tag, Description } from './Home.styles'
 import Sidebar from '../Sidebar/Sidebar'
 import axios from 'axios'
+import { Switch } from '@material-ui/core/'
 
 const AppStyles = () => {
   const app = document.querySelector('#root')
+  app.height = 'auto'
 
   app.style.display = 'grid'
   app.style.gridTemplateRows = '.5fr 1fr 1fr 1fr 1fr'
@@ -18,28 +20,68 @@ const AppStyles = () => {
   `
 }
 
-const fetchposts = async () => {
-  const user_id = "604acd00433005638077804a"
-  
-  const res = await axios.get(`/kipper/${user_id}`)
-  console.log(res.data)
-}
+const dummyText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+
+
 
 
 const Home = () => {
+  const [userPosts, changeUserPosts] = useState([{
+    likes: 0, 
+    tags: [''], 
+    post_id: '', 
+    timestamp: '', 
+    snippet: '', 
+    private: false,
+  }])
 
-  useEffect( fetchposts, [])
+  useEffect( () => {
+    AppStyles()
+    fetchposts()
+  }, [])
 
-  AppStyles()
+  const fetchposts = async () => {
+    const user_id = "604acd00433005638077804a"
+    
+    const res = await axios.get(`/kipper/${user_id}`)
+    changeUserPosts(res.data.data)
+  }
+
+  const addASnippet = (e) => {
+
+  }
+
+
   return (
     <>
-    <Title>Code Kipper</Title>
-    <Sidebar />
-    <Container>
-      
-      Home
-    </Container>
-    <Sidecard>sidecard</Sidecard>
+      <Title>Code Kipper</Title>
+      <Sidebar />
+      <PostsContainer>
+          {userPosts.map( (post, idx) => (
+            <Post key={idx}>
+              <Username>Ashley Pean</Username>
+              
+              <Pre>
+                <Code>
+                  {post.snippet}
+                </Code>
+              </Pre>
+              <Description>
+                {post?.description || dummyText}
+              </Description>
+              <Likes>{post?.likes || '0'} likes</Likes>
+              {post?.tags.map((el, idx) => (
+                <Tag>{el}</Tag>
+              ))}
+              <Switch checked={post.private} />
+
+            </Post>
+          ))}
+      </PostsContainer>
+
+      <Sidecard>
+        <button onClick={addASnippet}>Add a snippet</button>
+      </Sidecard>
     </>
 
   )
--- client/src/components/Home/Home.styles.js
+++ client/src/components/Home/Home.styles.js
@@ -1,9 +1,14 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
+export const PostsContainer = styled.div`
   grid-area: home;
-  border: 1px solid green;`
-Container.displayName = 'Container'
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem 0;
+  padding: 2rem 0;`
+PostsContainer.displayName = 'PostsContainer'
 
 
 export const Title = styled.h1`
@@ -17,3 +22,43 @@ export const Sidecard = styled.div`
   grid-area: sidecard;
   border: 1px solid blue;`
 Sidecard.displayName = 'Sidecard'
+
+
+export const Post = styled.div`
+  border: 1px solid red;
+  width: 80%;
+  padding: 1rem;`
+Post.displayName = 'Post'
+
+export const Username = styled.p`
+  font-weight: bold;
+  font-size: 1.4rem;`
+Username.displayName = 'Username'
+
+export const Pre = styled.pre`
+  `
+Pre.displayName = 'Pre'
+
+export const Code = styled.code`
+  width: 100%;
+  background: black;
+  color: white;
+  padding: 1rem;
+  border-radius: 4px;
+  overflow: scroll;
+  word-wrap: break-word;`
+Code.displayName = 'Code'
+
+export const Likes = styled.p`
+  display: inline;
+  margin: 0 1rem;`
+Likes.displayName = 'Likes'
+
+export const Tag = styled.span`
+  margin: 0 .5rem;`
+Tag.displayName = 'Tag'
+
+export const Description = styled.p`
+  `
+Description.displayName = 'Description'
+
