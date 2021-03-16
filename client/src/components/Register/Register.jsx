d476ec881b4eb6628d372cf9eeca3e3f540e7328
Ashley Pean <pean.ashley@gmail.com>
2021-03-16 14:31:31 -0400

    refactor register component

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -3,19 +3,9 @@ import { useHistory } from 'react-router-dom'
 import { Container, FormInput, SubmitButton, Label, Title, RegisterText } from './Register.styles'
 import axios from 'axios'
 
-const setAppStyles = () => {
-  const app = document.querySelector('#root')
-
-  app.style.display = 'flex'
-  app.style.justifyContent = 'center'
-  app.style.alignItems = 'center'
-}
 
 const Register = () => {
-  const nameInput = useRef()
-  const emailInput = useRef()
-  const passwordInput = useRef()
-  const confirmPasswordInput = useRef()
+  const nameInput = useRef(), emailInput = useRef(), confirmPasswordInput = useRef(), passwordInput = useRef()
   const history = useHistory()
   setAppStyles()
 
@@ -23,27 +13,18 @@ const Register = () => {
     e.preventDefault()
     const passwordsMatch = confirmPassword()
     if(passwordsMatch) {
-      const email = emailInput.current.value
-      const password = passwordInput.current.value
-      const name = nameInput.current.value
-      const response = await axios.post('/register', { name, email, password })
-      console.log(response)
-      console.log(response.data)
+      const response = await axios.post('/register', { 
+        name: nameInput.current.value, 
+        email: emailInput.current.value, 
+        password: passwordInput.current.value 
+      })
       response.status === 200 ? history.push('/') : window.alert('Error registering user')
-    }else {
-
     }
   }
 
   const confirmPassword = (e) => {
-    const password1 = passwordInput.current.value
-    const password2 = confirmPasswordInput.current.value
-    if(password1 === password2) {
-      console.log('passwords match')
-    }else{
-      //Error message
-      console.log(password1, password2)
-      console.log('passwords don\'t match')
+    const password1 = passwordInput.current.value, password2 = confirmPasswordInput.current.value
+    if(password1 !== password2) {
       window.alert('passwords do not match!')
       passwordInput.current.value = ''
       confirmPasswordInput.current.value = ''
@@ -53,18 +34,32 @@ const Register = () => {
   return (
     <Container onSubmit={handleSubmit}>
       <Title>Code Kipper</Title>
+
       <Label>Name</Label>
       <FormInput type="text" placeholder="name" ref={nameInput} />
+
       <Label>Email</Label>
       <FormInput type="text" placeholder="email" ref={emailInput} />
+
       <Label>Password</Label>
       <FormInput type="password" placeholder="password" ref={passwordInput}/>
+
       <Label>Confirm Password</Label>
       <FormInput type="password" placeholder="confirm password" ref={confirmPasswordInput} />
-      <SubmitButton type="submit">Submit</SubmitButton>
+
+      <SubmitButton type="submit" onSubmit={handleSubmit} onClick={handleSubmit}>Submit</SubmitButton>
+
       <RegisterText>Already registered? <a href="/login">Login now</a></RegisterText>
     </Container>
   )
+}
+
+const setAppStyles = () => {
+  const app = document.querySelector('#root')
+
+  app.style.display = 'flex'
+  app.style.justifyContent = 'center'
+  app.style.alignItems = 'center'
 }
 
 export default Register
