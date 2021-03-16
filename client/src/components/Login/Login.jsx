758fe9fdf5de8e3e4588a5f92c0870efcfe09129
Ashley Pean <pean.ashley@gmail.com>
2021-03-16 14:35:00 -0400

    refactor login

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -3,14 +3,6 @@ import { useHistory } from 'react-router-dom'
 import { Container, FormInput, SubmitButton, Label, Title, RegisterText } from './Login.styles'
 import axios from 'axios'
 
-const setAppStyles = () => {
-  const app = document.querySelector('#root')
-
-  app.style.display = 'flex'
-  app.style.justifyContent = 'center'
-  app.style.alignItems = 'center'
-}
-
 const Login = () => {
   const emailInput = useRef()
   const passwordInput = useRef()
@@ -19,34 +11,38 @@ const Login = () => {
   
   const handleSubmit = async (e) => {
     e.preventDefault()
-    const email = emailInput.current.value
-    const password = passwordInput.current.value
+    const email = emailInput.current.value, password = passwordInput.current.value
 
     const response = await axios.post('/login', {email, password})
-    console.log(response)
-    console.log(response.data)
-    if(response.status === 200) {
-      //redirect
-      history.push('/')
-    }else {
-      //show error message and ask to sign in again 
-      window.alert('Incorrect login, please try again')
-    }
-    
+
+    if(response.status === 200)  history.push('/')
+    else window.alert('Incorrect login, please try again')
   }
 
 
   return (
     <Container onSubmit={handleSubmit}>
       <Title>Code Kipper</Title>
+
       <Label>Email</Label>
       <FormInput type="text" placeholder="email" ref={emailInput} />
+
       <Label>Password</Label>
       <FormInput type="password" placeholder="password" ref={passwordInput}/>
+
       <SubmitButton type="submit">Submit</SubmitButton>
+
       <RegisterText>Not registered? <a href="/register">Create an account</a></RegisterText>
     </Container>
   )
+}
+
+const setAppStyles = () => {
+  const app = document.querySelector('#root')
+
+  app.style.display = 'flex'
+  app.style.justifyContent = 'center'
+  app.style.alignItems = 'center'
 }
 
 export default Login
