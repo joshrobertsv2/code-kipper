e23de35437619d297d0ca6d60933da82ec656f76
Ashley Pean <pean.ashley@gmail.com>
2021-03-25 03:12:56 -0400

    defer to header component

--- client/src/components/Feed/Feed.jsx
+++ client/src/components/Feed/Feed.jsx
@@ -1,26 +1,13 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom'
 import * as styles from './Feed.styles'
 import Sidebar from '../Sidebar/Sidebar'
-import NotificationsIcon from '@material-ui/icons/Notifications'
+import Header from '../Header/Header'
 
 const Feed = () => {
-  const location = useLocation()
-  const currentTabOpts = {
-    '/': 'Dashboard', 
-    '/feed': 'Feed',
-  }
+
   return (
     <>
-      <styles.Header>
-        <styles.CurrentTab>{currentTabOpts[location.pathname]}</styles.CurrentTab>
-        <styles.SearchBar placeholder="Search here"></styles.SearchBar>
-        <styles.Title>CodeKipper</styles.Title>
-        <styles.IconsContainer>
-          <NotificationsIcon fontSize="large"/>
-          <styles.Profile>A</styles.Profile>
-        </styles.IconsContainer>
-      </styles.Header>
+      <Header/>
       <Sidebar />
       <styles.Container>
         Coming soon!
