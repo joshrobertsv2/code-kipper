9c5bb04f4e41b3ee2d1b7c619be640766496876d
Ashley Pean <pean.ashley@gmail.com>
2021-03-26 11:03:15 -0400

    returned error

--- client/src/redux/saga/createPost/createPost.js
+++ client/src/redux/saga/createPost/createPost.js
@@ -4,18 +4,14 @@ import * as action from '../../actions/actions'
 import API from './api'
 
 export function* watchForNewPost() {
-  console.log('watching for new post')
   try{
     yield takeEvery(actionType.CREATE_USER_POST, createPost)
   }catch(err) {
-
+    return err
   }
 }
 
 function* createPost({payload}) {
-  console.log('creating new post')
-  console.log(payload)
-
   try {
     yield call(API.createPost, payload)
     yield put(action.updateAfterCreate({post: payload}))
